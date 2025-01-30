196117a2cc09e8f8216674803c96d23b7805105d
xLTJ <lucilorate@proton.me>
2025-01-30 23:01:32 +0100

    day 7 part 2

--- Solutions/day7/lib/lib_test.go
+++ Solutions/day7/lib/lib_test.go
@@ -33,9 +33,49 @@ func TestCalculateTotalCalibration(t *testing.T) {
 	}
 }
 
+func TestCalculateTotalCalibration2(t *testing.T) {
+	type args struct {
+		fileName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "Example input",
+			args:    args{fileName: "testInput.txt"},
+			want:    11387,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CalculateTotalCalibration2(tt.args.fileName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CalculateTotalCalibration2() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("CalculateTotalCalibration2() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func BenchmarkCalculateTotalCalibration(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := CalculateTotalCalibration("../Input.txt")
+		if err != nil {
+			return
+		}
+	}
+}
+
+func BenchmarkCalculateTotalCalibration2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := CalculateTotalCalibration2("../Input.txt")
 		if err != nil {
 			return
 		}
--- Solutions/day7/lib/part2.go
+++ Solutions/day7/lib/part2.go
@@ -1,1 +1,74 @@
 package lib
+
+import (
+	"math"
+	"strconv"
+	"strings"
+	"utils"
+)
+
+func CalculateTotalCalibration2(fileName string) (int, error) {
+	calibrations, err := utils.ParseInput(fileName, parseLine)
+	if err != nil {
+		return 0, err
+	}
+
+	result := 0
+	for _, calibration := range calibrations {
+		if isCalibrationValid2(calibration) {
+			result += calibration.result
+		}
+	}
+
+	return result, err
+}
+
+func isCalibrationValid2(calibration Calibration) bool {
+	operatorAmount := len(calibration.numbers) - 1
+
+	for i := 0; i < int(math.Pow(3, float64(operatorAmount))); i++ {
+
+		// same as in part 1, but with a base 3 instead cus we have 3 operators now
+		trinary := strconv.FormatInt(int64(i), 3)
+		if len(trinary) < operatorAmount {
+			trinary = strings.Repeat("0", operatorAmount-len(trinary)) + trinary
+		}
+		sum := calibration.numbers[0]
+
+		for j := range calibration.numbers {
+			// as we check for j + 1 calibration number, we dont need to last one
+			if j == operatorAmount {
+				break
+			}
+
+			switch trinary[j] {
+			case '0':
+				sum += calibration.numbers[j+1]
+			case '1':
+				sum *= calibration.numbers[j+1]
+			case '2':
+				sum = concatenateInts(sum, calibration.numbers[j+1])
+			}
+		}
+
+		if sum == calibration.result {
+			return true
+		}
+	}
+	return false
+}
+
+func concatenateInts(a int, b int) int {
+	multiplier := 1
+	tempB := b
+
+	for tempB > 0 {
+		multiplier *= 10
+		tempB /= 10
+	}
+	if b == 0 {
+		multiplier = 10
+	}
+
+	return a*multiplier + b
+}
